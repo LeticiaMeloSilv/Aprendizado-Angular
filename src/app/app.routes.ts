22937c0ffdbd0b03bb7bc369b12d54bf23e8a7ad
Leticia Melo Silva <katsu.nahero@gmail.com>
2025-03-07 07:27:02 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+// import { Routes } from '@angular/router';
+// import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+// export const routes: Routes = [
+// 	{path:'home', component: HomeComponent}
+// ];
+// src/app/app-routing.module.ts
+
+
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },  // Rota para o componente Home
+//   { path: '', redirectTo: '/home', pathMatch: 'full' }  // Redireciona para Home por padrão
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], // Registra as rotas no Angular
+  exports: [RouterModule]  // Torna as rotas acessíveis aos outros módulos
+})
+export class AppRoutingModule {}
