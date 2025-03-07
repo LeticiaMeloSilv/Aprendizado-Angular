2fed0932dd110170957dbb6b6bfe01d315f0ecbe
Lehh <123740554+LeticiaMeloSilv@users.noreply.github.com>
2025-03-07 15:28:05 -0300

    rotas arrumadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,24 +1,10 @@
-// import { Routes } from '@angular/router';
-// import { HomeComponent } from './components/home/home.component';
-
-// export const routes: Routes = [
-// 	{path:'home', component: HomeComponent}
-// ];
-// src/app/app-routing.module.ts
-
-
-// src/app/app-routing.module.ts
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SegundaTelaComponent } from './components/segunda-tela/segunda-tela.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },  // Rota para o componente Home
-//   { path: '', redirectTo: '/home', pathMatch: 'full' }  // Redireciona para Home por padrão
+	{path:'home', component: HomeComponent},
+  {path:'segunda', component: SegundaTelaComponent},
+  // {path:'', component: HomeComponent}
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)], // Registra as rotas no Angular
-  exports: [RouterModule]  // Torna as rotas acessíveis aos outros módulos
-})
-export class AppRoutingModule {}
+// src/app/app-routing.module.ts
---
+++ src/app/components/segunda-tela/segunda-tela.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segunda-tela',
+  imports: [],
+  templateUrl: './segunda-tela.component.html',
+  styleUrl: './segunda-tela.component.css'
+})
+export class SegundaTelaComponent {
+
+}
